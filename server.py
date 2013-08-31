37532150fa8dfed2885d944ba38d804789bad341
Brian Wiborg <baccenfutter@c-base.org>
2013-08-31 15:38:07 +0200

    refactoring server.update()

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import asyncore
 import logging
 import socket
+import time
 
 from neodym.connection import Connection
 from neodym.exceptions import NotYetInitialized
@@ -29,7 +30,7 @@ class Server(asyncore.dispatcher):
         self.logger.debug('Vanishing!')
 
     def server_activate(self):
-        self.logger.debug('Binding on %s (max: %s)' % (self.address,
+        self.logger.info('Binding on %s (max: %s)' % (self.address,
                                                        self.__max__))
         self.listen(self.__max__)
 
@@ -48,30 +49,34 @@ class Server(asyncore.dispatcher):
         asyncore.poll2()
         pass
 
-        for c in [c for c in Connection.__all__ if not c.is_connected]:
-            if not c.recv_queue.empty():
-                message = c.recv_queue.get()
-                self.logger.debug('Received message: %s' % message)
+        connections = [c for c in Connection.__all__]
+        if connections:
+            for c in connections:
+                if not c.recv_queue.empty():
+                    message = c.recv_queue.get()
+                    self.logger.debug('Received message: %s' % message)
 
-                # perform handshake operation on new connections
-                if c.is_connected is False:
-                    if message.unique_identifier == 'handshake':
-                        self.logger.debug('Handling handshake request...')
-                        if message.get_attr('msg_map_hash') == self.__hash__:
-                            self.logger.info('Handshake: OK')
-                            c.is_connected = True
-                            c.put(message)
-                            c.handle_write()
-                        else:
-                            self.logger.info('Hash mismatch!')
+                    # perform handshake operation on new connections
+                    if c.is_connected is False:
+                        if message.unique_identifier == 'handshake':
+                            self.logger.debug('Handling handshake request...')
+                            if message.get_attr('msg_map_hash') == self.__hash__:
+                                self.logger.info('Handshake: OK -> Connection-%s' % id(c))
+                                c.is_connected = True
+                                c.put(message)
+                                c.handle_write()
+                            else:
+                                self.logger.info('Hash mismatch!')
 
-                # handle the message for all connected clients
-                elif c.is_connected is True:
-                    # todo: handle message
-                    self.logger.info(message)
+                    # handle the message for all connected clients
+                    elif c.is_connected is True:
+                        # todo: handle message
+                        c.logger.info(message)
 
-                else:
-                    self.logger.debug('Internal server error!')
+                    else:
+                        self.logger.debug('Internal server error!')
+        else:
+            time.sleep(0.1)
 
     def serve_forever(self):
         while True:
