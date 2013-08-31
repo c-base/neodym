23e288e317b643c6e22b0768f819562fc96355e0
Brian Wiborg <baccenfutter@c-base.org>
2013-08-31 22:35:15 +0200

    add license stanza

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,24 @@
 """neodym.exception
 
-This module contains the neodym specific exceptions then may be raised while
-running neodym.
+Neodym - A thin message-bus wrapper around asyncore.
+Copyright (C) 2013  Brian Wiborg <baccenfutter@c-base.org>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 __author__ = "Brian Wiborg <baccenfutter@c-base.org"
 __date__ = "2013/08/31"
+__license__ = "GNU/AGPL-3.0"
 
 class NeodymException(Exception):
     pass
