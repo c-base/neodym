43f8faca18f360c0319694a12c3bc929d37216a5
Brian Wiborg <baccenfutter@c-base.org>
2013-08-31 16:10:25 +0200

    docstrings and comments

--- client.py
+++ client.py
@@ -1,4 +1,30 @@
-""""""
+"""neodym.client
+
+This module contains all routines for the neodym client.
+
+The client connects to a server and tries to establish a connection by
+performing a handshaking procedure. If the handshake completes
+successfully, the client can send messages to the server.
+
+
+Example:
+========
+
+import neodym
+import logging
+
+neodym.log_level = logging.DEBUG
+baseConfig = neodym.baseConfig()
+logging.baseConfig(**baseConfig)
+
+neodym.init()
+
+client = neodym.Client(host='127.0.0.1', port=12345)
+client.client_connect()
+
+connection = client.connection()
+print connection
+"""
 __author__ = "Brian Wiborg <baccenfutter@c-base.org"
 __date__ = "2013/08/31"
 
@@ -11,9 +37,15 @@ from neodym.message import Message
 
 
 class Client(asyncore.dispatcher):
+    """neodym.client.Client
+    """
     __hash__ = ""
 
     def __init__(self, host, port):
+        """
+        :param host:    hostname or ip of server as string
+        :param port:    TCP port as integer
+        """
         self.host = host
         self.port = port
         asyncore.dispatcher.__init__(self)
@@ -26,25 +58,44 @@ class Client(asyncore.dispatcher):
         self.__connection = None
 
     def handle_connect(self):
+        """asyncore.handle_connect
+        """
         self.logger.info('Connected to server, establishing connection...')
         self.__connection = Connection(self.socket, self)
 
     def handle_close(self):
+        """asyncore.handle_close
+        """
         self.logger.info('Closing.')
         Connection.__all__ = set()
 
     def client_connect(self):
+        """neodym.client.Client.client_connect
+
+        Connect to server.
+        """
         self.connect((self.host, self.port))
         pass
         self.update()
         pass
 
     def update(self):
+        """neodym.client.Client.update
+
+        Poll asyncore, pass and return.
+        """
         asyncore.poll2()
         pass
         return
 
     def connection(self):
+        """neodym.client.Client.connection
+
+        Obtain an established connection from the client by performing the
+        handshaking procedure with the server.
+
+        :returns neodym.connection.Connection:  the connection instance
+        """
         handshake = Message('handshake', [self.__hash__])
         self.__connection.put(handshake)
 
